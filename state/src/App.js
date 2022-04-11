84616df3d64c03b12cc5afeaf932a09362d36366
Rannith <kmrannithvikram451@gmail.com>
2022-04-11 11:44:40 +0530

    commit

---
+++ state/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Message from './Components/state';
+
+function App() {
+  return (
+    <div className="App">
+      <Message></Message>
+    </div>
+  );
+}
+
+export default App;
