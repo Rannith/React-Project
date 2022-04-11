3d60e07cc4536d3f594eff84b1b40b5c9baa09c4
Rannith <kmrannithvikram451@gmail.com>
2022-04-11 13:53:25 +0530

    commit

---
+++ components/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import First from './components/First';
+import Second from './components/Second';
+
+function App() {
+  return (
+    <div className="App">
+      <First></First>
+      <Second />
+    </div>
+  );
+}
+
+export default App;
